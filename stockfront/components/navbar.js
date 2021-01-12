18bd404b05cc55286a7147816f5949b4767661bf
IcarusVi <zscyrus31@gmail.com>
2021-01-12 18:09:09 -0500

    Added media query for tablets in NavBar

--- stockfront/components/navbar.js
+++ stockfront/components/navbar.js
@@ -3,20 +3,20 @@ import SearchIcon from '@material-ui/icons/Search';
 import WorkRoundedIcon from '@material-ui/icons/WorkRounded';
 import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
-
+import Link from 'next/link'
 
 import styles from '../styles/navbar.module.scss'
-import {useState} from 'react'
+import { useState } from 'react'
 
 const NavBar = () => {
-    const [display, setDisplay] = useState(true)
+    const [display, setDisplay] = useState(false)
 
-    const toggleDisplay = () =>{
-        if(display){
+    const toggleDisplay = () => {
+        if (display) {
             setDisplay(false)
             console.log(display)
         }
-        else{
+        else {
             setDisplay(true)
             console.log(display)
         }
@@ -25,15 +25,21 @@ const NavBar = () => {
 
     return (
         <div className={styles.nav}>
-            <MenuIcon onClick = {toggleDisplay} />
+            <MenuIcon className={styles.menuIcon} onClick={toggleDisplay} />
+            <div className={styles.logo}></div>
             <div className={styles.searchBar}>
                 <SearchIcon fontSize="small" />
                 <input type="text" placeholder="Browse stocks/companies"></input>
             </div>
-            <ul  className={`${styles.subMenu} ${display ? styles.active : ''}`}>
-                <li>Home</li>
-                <li>Portfolios</li>
-                <li>Profile</li>
+            <ul className={`${styles.subMenu} ${display ? styles.active : ''}`}>
+                <li> <HomeRoundedIcon fontSize="small" /> Home </li>
+                <li> <WorkRoundedIcon fontSize="small" /> Portfolios</li>
+                <li> <PersonRoundedIcon fontSize="small" /> Profile</li>
+            </ul>
+            <ul className={styles.menuTablet}>
+                <li> <HomeRoundedIcon fontSize="large" /> </li>
+                <li> <WorkRoundedIcon fontSize="large" /> </li>
+                <li> <PersonRoundedIcon fontSize="large" /> </li>
             </ul>
         </div>
     )
