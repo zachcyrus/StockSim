4e70b58d424307d4607290cd417cd9043e850109
IcarusVi <zscyrus31@gmail.com>
2021-01-12 00:45:47 -0500

    Added css for mobile navbar and made a dropdown

--- stockfront/components/navbar.js
+++ stockfront/components/navbar.js
@@ -1,17 +1,42 @@
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import WorkRoundedIcon from '@material-ui/icons/WorkRounded';
+import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+
+
 import styles from '../styles/navbar.module.scss'
+import {useState} from 'react'
 
 const NavBar = () => {
+    const [display, setDisplay] = useState(true)
+
+    const toggleDisplay = () =>{
+        if(display){
+            setDisplay(false)
+            console.log(display)
+        }
+        else{
+            setDisplay(true)
+            console.log(display)
+        }
+    }
+
+
     return (
-        <div className = 'nav'>
-            <MenuIcon/>
-            <div className = 'search-bar'>
-                <SearchIcon/>
+        <div className={styles.nav}>
+            <MenuIcon onClick = {toggleDisplay} />
+            <div className={styles.searchBar}>
+                <SearchIcon fontSize="small" />
                 <input type="text" placeholder="Browse stocks/companies"></input>
-
             </div>
-
+            <ul  className={`${styles.subMenu} ${display ? styles.active : ''}`}>
+                <li>Home</li>
+                <li>Portfolios</li>
+                <li>Profile</li>
+            </ul>
         </div>
     )
 }
+
+export default NavBar
--- stockfront/pages/index.js
+++ stockfront/pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import NavBar from '../components/navbar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>StockSim</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <NavBar/>
     </div>
 
 
