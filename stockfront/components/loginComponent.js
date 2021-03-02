3f73e391b3e47ec2f871665c8ba1d2f5af3ad58f
IcarusVi <zscyrus31@gmail.com>
2021-03-01 21:59:29 -0500

    Fixed guest route

--- stockfront/components/loginComponent.js
+++ stockfront/components/loginComponent.js
@@ -9,16 +9,7 @@ import Image from 'next/image'
 const LoginCard = () => {
     const router = useRouter()
     let apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : process.env.NEXT_PUBLIC_APIURL
-    const guestLogin = async() => {
-        let guestLoggedIn = await axios
-            .get(`${apiUrl}/auth/guest`,
-            { withCredentials: true })
-        if(guestLoggedIn.data.message){
-            router.push('/')
-        }
-
-        
-    }
+    
     return (
         <div className={styles.loginForm}>
             <div className={styles.drawing}>
@@ -33,7 +24,7 @@ const LoginCard = () => {
 
                     <GoogleLoginButton onClick={() => router.push(`${apiUrl}/auth/google`)} />
 
-                    <button onClick={guestLogin} className={styles.guestLogin}>
+                    <button onClick={() => router.push(`${apiUrl}/auth/guest`)} className={styles.guestLogin}>
                         <h2>Login as guest here</h2>
                     </button>
                 </div>
